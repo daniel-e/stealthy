3d622e1d2caa07de912dae0ada05335c28ecdcc1
Ano Nym <detzold@gmail.com>
2015-05-31 22:29:38 +0200

    splitter

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -4,7 +4,7 @@ extern crate libc;
 
 mod packet;
 mod tools;
-//pub mod splitter;
+pub mod splitter;
 
 use std::thread;
 use std::sync::Arc;
@@ -252,115 +252,4 @@ impl Network {
 
 
 
-// --------------------------------------------------
-
-extern crate rand;
-
-struct MessagePart {
-    buf: Vec<u8>,
-    seq: u32
-}
-
-struct MessageSplitter {
-    messages: Vec<MessagePart>,
-    id: u64,
-    ip: String
-}
-
-const MAX_MESSAGE_PART_SIZE: usize = 128;
-
-impl MessageSplitter {
-
-    pub fn new(msg: Message) -> MessageSplitter {
-        
-        let mut parts: Vec<MessagePart> = vec![];
-        let mut v: Vec<u8> = vec![];
-        let mut i: u32 = 0;
-
-        for p in 0..msg.buf.len() {
-            v.push(msg.buf[p]);
-            if v.len() == MAX_MESSAGE_PART_SIZE {
-                parts.push(MessagePart {
-                    buf: v.clone(),
-                    seq: i
-                });
-                i += 1;
-                v.clear();
-            }
-        }
-
-        if v.len() > 0 {
-            parts.push(MessagePart {
-                buf: v,
-                seq: i
-            });
-        }
-
-        MessageSplitter {
-            messages: parts,
-            id: rand::random::<u64>(),
-            ip: msg.ip
-        }
-    }
-}
-
-// ------------------------------------------------------------------------
-// TESTS
-// ------------------------------------------------------------------------
-
-#[cfg(test)]
-mod tests {
-
-    use super::Message;
-    use super::MessageSplitter;
-
-    #[test]
-    fn test_message_split_id() {
-        let m1 = MessageSplitter::new(Message::new("1.2.3.4".to_string(), vec![]));
-        let m2 = MessageSplitter::new(Message::new("1.2.3.4".to_string(), vec![]));
-        // it should be very unlikely that an ID is equal to zero
-        assert!(m1.id != 0);
-        assert!(m2.id != 0);
-        // check that messages have different IDs
-        assert!(m1.id != m2.id);
-    }
-
-    #[test]
-    fn test_message_split_small() {
-        
-        let data = "hallo".to_string().into_bytes();
-        let msg = Message::new("1.2.3.4".to_string(), data.clone());
-        let s = MessageSplitter::new(msg);
-
-        assert_eq!(s.ip, "1.2.3.4".to_string());
-        assert!(s.id != 0);
-        assert!(s.messages.len() == 1);
-        assert!(s.messages[0].seq == 0);
-        assert_eq!(s.messages[0].buf, data);
-    }
-
-    #[test]
-    fn test_message_split_big() {
-
-        let piece = "0123456789".to_string().into_bytes();
-        let mut data: Vec<u8> = Vec::new();
-        for _ in 0..20 {
-            for i in piece.clone() { data.push(i); }
-        }
-        let msg = Message::new("1.2.3.4".to_string(), data.clone());
-        let s = MessageSplitter::new(msg);
-
-        assert_eq!(s.ip, "1.2.3.4".to_string());
-        assert!(s.id != 0);
-        assert!(s.messages.len() == 2);
-        assert!(s.messages[0].seq == 0);
-        assert!(s.messages[1].seq == 1);
-        assert!(s.messages[0].buf.len() == super::MAX_MESSAGE_PART_SIZE);
-        assert!(s.messages[1].buf.len() == data.len() - super::MAX_MESSAGE_PART_SIZE);
-
-        let (v1, v2) = data.split_at(super::MAX_MESSAGE_PART_SIZE);
-        assert_eq!(s.messages[0].buf, v1);
-        assert_eq!(s.messages[1].buf, v2);
-    }
-}
 
